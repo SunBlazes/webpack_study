53f60928d692a006da75946420782ce0cdc6cd65
SunBlazes <986176132@qq.com>
2020-12-16 11:24:40 +0800

    shixi1

---
+++ main.js
@@ -0,0 +1,5 @@
+import './index.scss'
+import $ from 'jquery'
+
+console.log('878787454446545')
+console.log($)
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: './static/js/[name].[chunkhash:8].chunk.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: './static/css/[name].[contenthash:8].content.css'
+    }),
+    new BundleAnalyzerPlugin(),
+    new CompressionWebpackPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html).*$/i,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ],
+  devServer: {
+    port: 8000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+      },
+      {
+        test: /\.s(c|a)ss$/,
+        exclude: /(node_modules)/,
+        use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'sass-loader', {
+          loader: 'style-resources-loader',
+          options: {
+            patterns: [
+              path.resolve(__dirname, './src/style/*.scss')
+            ]
+          }
+        }]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // name is like D:\projects\extract-css\node_modules\_jquery@3.5.1@jquery\dist
+            const reg = /[\\/]node_modules[\\/](.*)?[\\/]/
+            const packageName = module.context.match(reg)[1]
+            return `npm.${packageName.replace(/@/g,'')}`
+          }
+        }
+      }
+    }
+  }
+}
